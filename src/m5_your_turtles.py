625a79e891fd89b51ce2db5734eadbe43edd88a5
bakereb <bakereb@rose-hulman.edu>
2017-11-27 18:33:32 -0500

    m5 Completed

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Ethan Baker.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,40 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red',5)
+red_turtle.speed = 10
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('blue',5)
+blue_turtle.speed = 10
+
+
+size = 100
+
+for k in range(8):
+    red_turtle.draw_circle(size)
+
+    red_turtle.pen_up()
+    red_turtle.right(45)
+    red_turtle.forward(5)
+    red_turtle.left(45)
+
+    blue_turtle.draw_circle(size)
+
+    blue_turtle.pen_up()
+    blue_turtle.left(45)
+    blue_turtle.backward(5)
+    blue_turtle.right(45)
+
+    red_turtle.pen_down()
+    blue_turtle.pen_down()
+    size = size + 2
+
+
+window.close_on_mouse_click()
